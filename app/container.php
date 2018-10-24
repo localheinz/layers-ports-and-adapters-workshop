43737566fd9dfca6f79a28d5bdda18dfc8973a1c
Andreas Möller <am@localheinz.com>
2018-10-24 14:06:41 +0200

    Fix: Imports

--- app/container.php
+++ app/container.php
@@ -3,26 +3,15 @@
 declare(strict_types=1);
 
 use Interop\Container\ContainerInterface;
+use MeetupOrganizing\Application;
 use MeetupOrganizing\Domain;
-use MeetupOrganizing\Infrastructure\Persistence\Filesystem\MeetupRepository;
-use MeetupOrganizing\Infrastructure\UserInterface\Console\Command\ScheduleMeetupConsoleHandler;
-use MeetupOrganizing\Infrastructure\UserInterface\Http\Controller\ListMeetupsController;
-use MeetupOrganizing\Infrastructure\UserInterface\Http\Controller\MeetupDetailsController;
-use MeetupOrganizing\Infrastructure\UserInterface\Http\Controller\ScheduleMeetupController;
-use MeetupOrganizing\Infrastructure\UserInterface\Http\Views\TwigTemplates;
+use MeetupOrganizing\Infrastructure;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 use Symfony\Component\Debug\Debug;
 use Symfony\Component\Debug\ErrorHandler;
 use Xtreamwayz\Pimple\Container;
-use Zend\Expressive\Application;
-use Zend\Expressive\Container\ApplicationFactory;
-use Zend\Expressive\Helper\ServerUrlHelper;
-use Zend\Expressive\Helper\UrlHelper;
-use Zend\Expressive\Router\FastRouteRouter;
-use Zend\Expressive\Router\RouterInterface;
-use Zend\Expressive\Template\TemplateRendererInterface;
-use Zend\Expressive\Twig\TwigRendererFactory;
+use Zend\Expressive;
 
 Debug::enable();
 ErrorHandler::register();
@@ -34,7 +23,7 @@ $container['config'] = [
     'templates' => [
         'extension' => 'html.twig',
         'paths' => [
-            TwigTemplates::getPath(),
+            Infrastructure\UserInterface\Http\Views\TwigTemplates::getPath(),
         ],
     ],
     'twig' => [
@@ -45,19 +34,19 @@ $container['config'] = [
         [
             'name' => 'list_meetups',
             'path' => '/',
-            'middleware' => ListMeetupsController::class,
+            'middleware' => Infrastructure\UserInterface\Http\Controller\ListMeetupsController::class,
             'allowed_methods' => ['GET'],
         ],
         [
             'name' => 'meetup_details',
             'path' => '/meetup/{id}',
-            'middleware' => MeetupDetailsController::class,
+            'middleware' => Infrastructure\UserInterface\Http\Controller\MeetupDetailsController::class,
             'allowed_methods' => ['GET'],
         ],
         [
             'name' => 'schedule_meetup',
             'path' => '/schedule-meetup',
-            'middleware' => ScheduleMeetupController::class,
+            'middleware' => Infrastructure\UserInterface\Http\Controller\ScheduleMeetupController::class,
             'allowed_methods' => ['GET', 'POST'],
         ],
     ],
@@ -73,64 +62,64 @@ $container['Zend\Expressive\FinalHandler'] = function () {
         }
     };
 };
-$container[RouterInterface::class] = function () {
-    return new FastRouteRouter();
+$container[Expressive\Router\RouterInterface::class] = function () {
+    return new Expressive\Router\FastRouteRouter();
 };
-$container[Application::class] = new ApplicationFactory();
+$container[Expressive\Application::class] = new Expressive\Container\ApplicationFactory();
 
 /*
  * Templating
  */
-$container[TemplateRendererInterface::class] = new TwigRendererFactory();
-$container[ServerUrlHelper::class] = function () {
-    return new ServerUrlHelper();
+$container[Expressive\Template\TemplateRendererInterface::class] = new Expressive\Twig\TwigRendererFactory();
+$container[Expressive\Helper\ServerUrlHelper::class] = function () {
+    return new Expressive\Helper\ServerUrlHelper();
 };
-$container[UrlHelper::class] = function (ContainerInterface $container) {
-    return new UrlHelper($container[RouterInterface::class]);
+$container[Expressive\Helper\UrlHelper::class] = function (ContainerInterface $container) {
+    return new Expressive\Helper\UrlHelper($container[Expressive\Router\RouterInterface::class]);
 };
 
 /*
  * Persistence
  */
 $container[Domain\Entity\MeetupRepository::class] = function () {
-    return new MeetupRepository(__DIR__ . '/../var/meetups.txt');
+    return new Infrastructure\Persistence\Filesystem\MeetupRepository(__DIR__ . '/../var/meetups.txt');
 };
 
 /**
  * Application.
  */
-$container[\MeetupOrganizing\Application\Command\ScheduleMeetupHandler::class] = function (ContainerInterface $container) {
-    return new \MeetupOrganizing\Application\Command\ScheduleMeetupHandler($container->get(Domain\Entity\MeetupRepository::class));
+$container[Application\Command\ScheduleMeetupHandler::class] = function (ContainerInterface $container) {
+    return new Application\Command\ScheduleMeetupHandler($container->get(Domain\Entity\MeetupRepository::class));
 };
 
 /*
  * Controllers
  */
-$container[ScheduleMeetupController::class] = function (ContainerInterface $container) {
-    return new ScheduleMeetupController(
-        $container->get(TemplateRendererInterface::class),
-        $container->get(RouterInterface::class),
-        $container->get(\MeetupOrganizing\Application\Command\ScheduleMeetupHandler::class)
+$container[Infrastructure\UserInterface\Http\Controller\ScheduleMeetupController::class] = function (ContainerInterface $container) {
+    return new Infrastructure\UserInterface\Http\Controller\ScheduleMeetupController(
+        $container->get(Expressive\Template\TemplateRendererInterface::class),
+        $container->get(Expressive\Router\RouterInterface::class),
+        $container->get(Application\Command\ScheduleMeetupHandler::class)
     );
 };
-$container[ListMeetupsController::class] = function (ContainerInterface $container) {
-    return new ListMeetupsController(
+$container[Infrastructure\UserInterface\Http\Controller\ListMeetupsController::class] = function (ContainerInterface $container) {
+    return new Infrastructure\UserInterface\Http\Controller\ListMeetupsController(
         $container->get(Domain\Entity\MeetupRepository::class),
-        $container->get(TemplateRendererInterface::class)
+        $container->get(Expressive\Template\TemplateRendererInterface::class)
     );
 };
-$container[MeetupDetailsController::class] = function (ContainerInterface $container) {
-    return new MeetupDetailsController(
+$container[Infrastructure\UserInterface\Http\Controller\MeetupDetailsController::class] = function (ContainerInterface $container) {
+    return new Infrastructure\UserInterface\Http\Controller\MeetupDetailsController(
         $container->get(Domain\Entity\MeetupRepository::class),
-        $container->get(TemplateRendererInterface::class)
+        $container->get(Expressive\Template\TemplateRendererInterface::class)
     );
 };
 
 /*
  * CLI
  */
-$container[ScheduleMeetupConsoleHandler::class] = function (ContainerInterface $container) {
-    return new ScheduleMeetupConsoleHandler($container->get(\MeetupOrganizing\Application\Command\ScheduleMeetupHandler::class));
+$container[Infrastructure\UserInterface\Console\Command\ScheduleMeetupConsoleHandler::class] = function (ContainerInterface $container) {
+    return new Infrastructure\UserInterface\Console\Command\ScheduleMeetupConsoleHandler($container->get(Application\Command\ScheduleMeetupHandler::class));
 };
 
 return $container;
